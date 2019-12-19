1bdea886cbd89e5e493ff6251e9943b449665d04
Pete Hanner <petehanner@gmail.com>
2019-12-19 16:03:20 -0500

    logic for letter swapping

--- index.js
+++ index.js
@@ -21,10 +21,52 @@ const convertText = (text) => {
 // flip uppercase to lowercase and vice versa
 // except ambiguous characters
 // don't touch non-letters
-
 const alternateCases = (chars) => {
+  const swapped = []
+  let nextCase = "lower"
 
+  for (const char of chars) {
+    if (char === 'l' || char === 'L') {
+      swapped.push('L');
+      nextCase = "lower"
+    } else if (char === 'i' || char === 'I') {
+      swapped.push('i');
+      nextCase = "upper";
+    } else if (char.match(/[a-z]/i)) {
+      if (nextCase === "lower") {
+        swapped.push(char.toLowerCase());
+        nextCase = "upper";
+      } else {
+        swapped.push(char.toUpperCase());
+        nextCase = "lower"
+      }
+    } else {
+      swapped.push(char);
+    }
+  }
+
+  return swapped
 }
+
+
+
+// switch (char) {
+//   case (char === 'l' || char === 'L'):
+//     swapped.push('L');
+//     nextCase = "lower"
+//     break;
+//   case (char === 'i' || char === 'I'):
+//     swapped.push('i');
+//     nextCase = "upper";
+//     break;
+//   case ()
+//       default:
+//     swapped.push(char);
+//     break;
+// }
+
+
+
 
 const handleSubmit = (e) => {
   e.preventDefault()
