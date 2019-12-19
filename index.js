1e79d7b5feebb7e1575c627f0cb44e470b0ef1b1
Pete Hanner <petehanner@gmail.com>
2019-12-19 10:40:26 -0500

    join("") also works fine

--- index.js
+++ index.js
@@ -2,23 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("Page loaded")
 })
 
+// Add text with letters swapped to DOM
 const showConvertedText = (text) => {
   const convertedText = convertText(text);
   const p = document.querySelector("#converted-text");
   p.innerText = convertedText;
 }
 
+// Use helper methods to turn inputted text 
 const convertText = (text) => {
   const letters = text.split("");
   const newLetters = swapCases(letters);
-  const newString = joinString(newLetters);
+  const newString = newLetters.join("");
   return newString;
 }
 
-const swapCases = (charArr) => {
+const swapCases = (letters) => {
   // flip cases of chars as applicable
-}
-
-const joinString = (charArr) => {
-  // turn arr of chars back into string
 }
