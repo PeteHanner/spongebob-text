15bab4f9d5de83d40af74eec4ad8a57b2f557862
Pete Hanner <petehanner@gmail.com>
2019-12-19 10:13:24 -0500

    top-level js function

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Page loaded")
 })
+
+const convertText = (text) => {
+  const letters = splitString(text);
+  const newLetters = swapCases(letters);
+  const newString = joinString(newLetters);
+}
