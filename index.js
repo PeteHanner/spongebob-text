7d8b7d4b95db191a487d56462c79931aedd0b1a5
Pete Hanner <petehanner@gmail.com>
2019-12-19 10:24:12 -0500

    initial js structure

--- index.js
+++ index.js
@@ -2,8 +2,27 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("Page loaded")
 })
 
+const showConvertedText = (text) => {
+  const convertedText = convertText(text);
+  const p = document.querySelector("#converted-text");
+  p.innerText = convertedText;
+}
+
 const convertText = (text) => {
   const letters = splitString(text);
   const newLetters = swapCases(letters);
   const newString = joinString(newLetters);
+  return newString;
+}
+
+const splitString = (text) => {
+  // divide string into characters
+}
+
+const swapCases = (charArr) => {
+  // flip cases of chars as applicable
+}
+
+const joinString = (charArr) => {
+  // turn arr of chars back into string
 }
